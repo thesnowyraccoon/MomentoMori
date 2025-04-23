dd89a7b60e2306020f9465c358daf1097501a800
Ndash-GHub <Muhuli.sivhaga@gmail.com>
2025-04-23 12:23:13 +0200

    Code reference

--- Prototype/Assets/Scripts/MainMenu.cs
+++ Prototype/Assets/Scripts/MainMenu.cs
@@ -1,5 +1,13 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
+//Title: Make Your MAIN MENU Quickly! | Unity UI Tutorial For Beginners
+//Author: Rehope Games 
+//Date: April 21 2025
+//Platform: Youtube
+//Code version: Unknown
+//Availability: https://youtu.be/DX7HyN7oJjE?si=s2ex07i9EnO4aECp
+
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
--- Prototype/Assets/Scripts/PauseMenu.cs
+++ Prototype/Assets/Scripts/PauseMenu.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
+//Title: Make Your MAIN MENU Quickly! | Unity UI Tutorial For Beginners
+//Author: Rehope Games 
+//Date: April 21 2025
+//Platform: Youtube
+//Code version: Unknown
+//Availability: https://youtu.be/DX7HyN7oJjE?si=s2ex07i9EnO4aECp
+
 public class PauseMenu : MonoBehaviour
 {
     public InputAction pauseAction;
