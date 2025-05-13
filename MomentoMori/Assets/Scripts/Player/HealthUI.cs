c4df404cb47015d95b9ed8a0d62887f770ec499c
Snowy <snowytheraccoon@gmail.com>
2025-05-13 22:19:44 +0200

    Health UI system

--- MomentoMori/Assets/Scripts/Player/HealthUI.cs
+++ MomentoMori/Assets/Scripts/Player/HealthUI.cs
@@ -16,6 +16,9 @@ public class HealthUI : MonoBehaviour
     private int maxHealth;
 
     public Sprite emptyStar;
+    public Sprite quarterStar;
+    public Sprite halfStar;
+    public Sprite threequarterStar;
     public Sprite fullStar;
     public Image[] stars;
 
@@ -24,18 +27,56 @@ public class HealthUI : MonoBehaviour
         health = player.playerHealth;
         maxHealth = player.maxHealth;
 
+        int maxCount = 0;
+
+        if (maxHealth == 50)
+        {
+            maxCount = 5;
+        }
+        else if (maxHealth == 40)
+        {
+            maxCount = 4;
+        }
+        else if (maxHealth == 30)
+        {
+            maxCount = 3;
+        }
+        else if (maxHealth == 20)
+        {
+            maxCount = 2;
+        }
+        else if (maxHealth == 10)
+        {
+            maxCount = 1;
+        }
+
+        float healthCheck = health/maxCount;
+        int maxCheck = maxHealth/maxCount;
+
         for (int i = 0; i < stars.Length; i++)
         {
-            if (i < health)
+            if (healthCheck == maxCheck)
             {
                 stars[i].sprite = fullStar;
             }
-            else
+            else if (healthCheck > (maxCheck/2))
+            {
+                stars[i].sprite = threequarterStar;
+            }
+            else if (healthCheck == (maxCheck/2))
+            {
+                stars[i].sprite = halfStar;
+            }
+            else if (healthCheck < (maxCheck/2))
+            {
+                stars[i].sprite = quarterStar;
+            }
+            else if (healthCheck == 0)
             {
                 stars[i].sprite = emptyStar;
             }
 
-            if (i < maxHealth)
+            if (i < maxCount)
             {
                 stars[i].enabled = true;
             }
