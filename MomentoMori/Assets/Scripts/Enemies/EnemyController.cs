07be118f1f30b77cccf8b9d34351ed5c8288feee
Snowy <snowytheraccoon@gmail.com>
2025-05-15 21:46:33 +0200

    Main up to date

--- MomentoMori/Assets/Scripts/Enemies/EnemyController.cs
+++ MomentoMori/Assets/Scripts/Enemies/EnemyController.cs
@@ -17,12 +17,6 @@ public class EnemyController : MonoBehaviour
     public float stamina = 5f;
     bool hasStamina = true;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
