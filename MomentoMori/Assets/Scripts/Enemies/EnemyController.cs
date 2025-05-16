c7ac6cf75c5a83d865912b0f17ea78e4648fd998
Snowy <snowytheraccoon@gmail.com>
2025-05-16 20:19:02 +0200

    successful merge

--- MomentoMori/Assets/Scripts/Enemies/EnemyController.cs
+++ MomentoMori/Assets/Scripts/Enemies/EnemyController.cs
@@ -1,68 +1,27 @@
-using TMPro;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
     // Player stats
     public PlayerController player;
-    private int playerHealth;
-    private int playerMaxHealth;
 
     // Enemy stats
-    public int health = 100;
-
+    public int health;
+    public int maxHealth = 100;
     public int attack = 5;
 
-    float cooldown = 0f;
-    public float stamina = 5f;
-    bool hasStamina = true;
-
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        Stamina();
-
-        playerHealth = player.playerHealth;
-        playerMaxHealth = player.maxHealth;
-
-        if (health <= 0)   // Checks health and destroys if dead
-        {
-            Destroy(gameObject); 
-        }
+        health = maxHealth;
     }
 
-    // Checks if player attacks and applies damage
-    void OnTriggerStay2D(Collider2D trigger)
+    void TakeDamage(int damage)
     {
-        if (trigger.gameObject.CompareTag("Player"))
-        {
-            health -= player.Attack();
+        health -= damage;
 
-            if (hasStamina)
-            {
-                playerHealth -= attack;
-                player.playerHealth = playerHealth;
-
-                hasStamina = false;
-            }
-        }
-    }
-
-    void Stamina()
-    {
-        if ((cooldown <= 0f) && (hasStamina == false))
+        if (health <= 0)
         {
-            hasStamina = true;
-            cooldown = stamina;
-        }
-        else
-        {
-            cooldown -= Time.deltaTime;
-            
-            if (cooldown < 0f)
-            {
-                cooldown = 0f;
-            }
+            Destroy(gameObject);
         }
     }
 }
