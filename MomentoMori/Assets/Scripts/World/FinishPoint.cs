043ff70f1c7c4a1c6de756218525ff239d244a52
SkullManea <venturousskull@gmail.com>
2025-05-30 12:44:41 +0200

    Basic Scene Change

---
+++ MomentoMori/Assets/Scripts/World/FinishPoint.cs
@@ -0,0 +1,24 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+
+public class FinishPoint : MonoBehaviour
+{
+    [SerializeField] bool goNextLevel;
+    [SerializeField] string levelName;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (goNextLevel)
+            {
+                SceneController.instance.NextLevel();
+            }
+            else
+            {
+                SceneController.instance.LoadScene(levelName); //Load Level By its name, Useful in terms of the player dying
+            }
+            
+        }
+    }
+
+}
---
+++ MomentoMori/Assets/Scripts/World/SceneController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public static SceneController instance; //Access from anywhere
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); //Doesnt destroy the empty object containg the script
+        }
+        else
+        {
+            Destroy(gameObject); //If scene already has this object it will destroy the previous one
+        }
+    }
+    public void NextLevel()
+    { //Load Next Scene. (+1 is to indicate the scene after it)
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadSceneAsync(sceneName);  //Go to the desired scene using the scene name
+    }
+}
+
