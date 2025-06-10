1543eec3e71fd62e2ad3ddafb810c8136903cdce
SkullManea <venturousskull@gmail.com>
2025-06-10 21:10:57 +0200

    sound

--- MomentoMori/Assets/Scripts/Sound/AudioPlayer.cs
+++ MomentoMori/Assets/Scripts/Sound/AudioPlayer.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class AudioPlayer : MonoBehaviour
 {
+    //The tutorial scene has a game object: SoundEffectManager, this has everything, just copy paste into new scenes
     private PlayerController player; //Refers to the player script (Death Sound)
     private Melatonin melatonin; //Refers to the melatonin script (Pickup Sound)
     private EnemyController enemy; //Refers to the enemies script (Owl laugh)
@@ -32,8 +33,9 @@ public class AudioPlayer : MonoBehaviour
         {
             Debug.Log("Ahhhhhhh");
         }*/
-
+        //When the owl enemy is following play this sound effect
         //following = enemy.Follow(); Enemy script needs to be unlocked D:
+        //  SoundEffectManager.Play("OwlLaugh")
 
     }
 }
