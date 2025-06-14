e25e1afd95d41e362f31bb919b07e6d18eb12998
SkullManea <venturousskull@gmail.com>
2025-06-14 21:49:58 +0200

    Basic Scene Transitions
    Still have to be added to every scene

--- MomentoMori/Assets/Scripts/World/SceneController.cs
+++ MomentoMori/Assets/Scripts/World/SceneController.cs
@@ -1,16 +1,18 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 // Title: Unity 2D SCENE MANAGEMENT tutorial
- // Author: Rehope Games
- // Date: 13 March 2023
- // Code version: Unknown
- // Availability: https://youtu.be/E25JWfeCFPA?si=jPe9A3gVatkBPRZE
+// Author: Rehope Games
+// Date: 13 March 2023
+// Code version: Unknown
+// Availability: https://youtu.be/E25JWfeCFPA?si=jPe9A3gVatkBPRZE
 
 public class SceneController : MonoBehaviour
 {
     public static SceneController instance; //Access from anywhere
                                             //Script Destroyer
+    [SerializeField] Animator transitionAnim;
     private void Awake()
     {
         if (instance == null)
@@ -25,12 +27,25 @@ public class SceneController : MonoBehaviour
     }
     public void NextLevel()
     { //Load Next Scene. (+1 is to indicate the scene after it)
-        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(LoadLevel());
     }
 
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadSceneAsync(sceneName);  //Go to the desired scene using the scene name
+    }
+
+// Title: Unity Scene Transitions: Creating an Immersive and seamless Gaming Experience
+// Author: Rehope Games
+// Date: 7 April 2023
+// Code version: Unknown
+// Availability: https://youtu.be/HBEStd96UzI?si=ffU8jJTDLNc6oTgo
+    IEnumerator LoadLevel()
+    {
+        transitionAnim.SetTrigger("End");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        transitionAnim.SetTrigger("Start");
     }
 }
 
